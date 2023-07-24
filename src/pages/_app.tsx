8b75d59a9c8d968e2ca317f25dabf2b512208484
James Redding <jdr161@case.edu>
2023-07-24 14:21:30 -0500

    add colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import '@/styles/globals.css'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { MantineProvider } from '@mantine/core'
+import { MantineProvider, Tuple, DefaultMantineColor } from '@mantine/core'
 import { emotionCache } from '../emotion-cache'
 import { Roboto_Flex } from 'next/font/google'
 const robotoFlex = Roboto_Flex({ subsets: ['latin'] })
 import Layout from '@/components/layout'
+
+type ExtendedCustomColors =
+  | 'light-blue'
+  | 'dark-blue'
+  | 'background-light'
+  | 'offwhite'
+  | DefaultMantineColor
+
+declare module '@mantine/core' {
+  export interface MantineThemeColorsOverride {
+    colors: Record<ExtendedCustomColors, Tuple<string, 10>>
+  }
+}
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props
@@ -24,9 +37,14 @@ export default function App(props: AppProps) {
         withGlobalStyles
         withNormalizeCSS
         theme={{
-          /** Put your mantine theme override here */
           colorScheme: 'light',
           fontFamily: robotoFlex.style.fontFamily,
+          colors: {
+            'light-blue': ['#4C768D'],
+            'dark-blue': ['#1C4C64'],
+            'background-light': ['#EDF1F4'],
+            offwhite: ['#EDF1F4'],
+          },
         }}
         emotionCache={emotionCache}
       >
