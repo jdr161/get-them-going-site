9117e244919cb8011580c57e81f756269cf53c25
James Redding <jdr161@case.edu>
2023-07-25 12:50:56 -0500

    add title and logo

---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,41 @@
+import { Container, Grid, Title, useMantineTheme } from '@mantine/core'
+import Image from 'next/image'
+import Logo from '@/images/GTG-logo-plain.png'
+
+export default function Navbar() {
+  const theme = useMantineTheme();
+  const navbarHeight = 75
+
+  return (
+    <Container
+      fluid={true}
+      sx={(theme) => ({
+        height: navbarHeight,
+        backgroundColor: theme.colors.backgroundLight,
+      })}
+    >
+      <Grid justify="center" align="center">
+        <Grid.Col span='content' py={7.5}>
+          <Image
+            height={navbarHeight-15}
+            src={Logo}
+            alt='Get them Going logo'
+          />
+        </Grid.Col>
+        <Grid.Col span='auto'>
+          <Title 
+            size={32} 
+            weight={700}
+            color={theme.colors.lightBlue[0]}
+            align='center'
+          >
+            Get Them Going
+          </Title>
+        </Grid.Col>
+        <Grid.Col span='content'>
+          buttons
+        </Grid.Col>
+      </Grid>
+    </Container>
+  )
+}
