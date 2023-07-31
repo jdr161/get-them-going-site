cbf02676b3419788cb9e82d08f361b6028ba9c48
James Redding <jdr161@case.edu>
2023-07-31 12:12:08 -0500

    refactor for readability

---
+++ src/components/Hero/HeroSection2/heroSection2.tsx
@@ -0,0 +1,26 @@
+import { Container, useMantineTheme, Title } from '@mantine/core'
+import HeroCarousel from '@/components/Hero/HeroSection2/HeroCarousel/heroCarousel'
+
+export default function HeroSection2() {
+  const heroSection2Height = 541
+  const theme = useMantineTheme()
+
+  return (
+    <Container
+      fluid={true}
+      h={heroSection2Height}
+      bg={theme.colors.darkBlue[0]}
+    >
+      <Title
+        color="white"
+        ta="center"
+        pt={29}
+        pb={44}
+        sx={{ fontSize: 40, fontStyle: 'normal', fontWeight: 700 }}
+      >
+        How We Support Students
+      </Title>
+      <HeroCarousel />
+    </Container>
+  )
+}
