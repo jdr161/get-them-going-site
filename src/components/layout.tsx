dda49ba7ad4dfed594df4b62a86264e4d46846f9
James Redding <jdr161@case.edu>
2023-08-01 12:33:39 -0500

    layout have at least full page as container

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,16 @@
 import Navbar from '@components/Navbar/navbar'
+import Footer from '@components/Footer/footer'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <nav><Navbar /></nav>
+    <div className="h-screen w-full">
+      <nav>
+        <Navbar />
+      </nav>
       <main>{children}</main>
-    </>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
